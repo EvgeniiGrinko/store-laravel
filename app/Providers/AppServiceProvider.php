c1840672ffdd011ba2ef8f72741a620b41ce0a12
Евгений Гринько <evgenijgrinko@MacBook-Air-Evgenij.local>
2022-01-06 22:07:59 +0300

    changed AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,7 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Schema::deafaultStringLength(191);
     }
 
     /**
